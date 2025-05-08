5b72ce0df5129f79412a867d9371fbebe9b2e534
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2025-05-08 14:00:44 +0300

    test

--- clang/test/compiler-course/sedova/test.cpp
+++ clang/test/compiler-course/sedova/test.cpp
@@ -11,77 +11,14 @@ bool isPositive(int a) {
 // CHECK-LABEL: FunctionDecl {{.*}} compute 'double (int, float)'
 // CHECK: ParmVarDecl {{.*}} 'int'
 // CHECK: ParmVarDecl {{.*}} 'float'
-// CHECK: BinaryOperator {{.*}} 'double' '+'
+// CHECK: ReturnStmt
+// CHECK: ImplicitCastExpr {{.*}} 'double' <FloatingCast>
+// CHECK: BinaryOperator {{.*}} 'float' '+'
 // CHECK: ImplicitCastExpr {{.*}} 'float' <IntegralToFloating>
 // CHECK: 'int' lvalue ParmVar
-// CHECK: ImplicitCastExpr {{.*}} 'double' <FloatingCast>
+// CHECK: ImplicitCastExpr {{.*}} 'float' <LValueToRValue>
 // CHECK: 'float' lvalue ParmVar
 
 double compute(int x, float y) {
   return x + y;
-}
-
-// CHECK-LABEL: FunctionDecl {{.*}} process 'int (float, float)'
-// CHECK: ParmVarDecl {{.*}} 'float'
-// CHECK: ParmVarDecl {{.*}} 'float'
-// CHECK: ReturnStmt
-// CHECK: ImplicitCastExpr {{.*}} 'int' <FloatingToIntegral>
-// CHECK: BinaryOperator {{.*}} 'double' '+'
-// CHECK: ImplicitCastExpr {{.*}} 'double' <FloatingCast>
-// CHECK: 'float' lvalue ParmVar
-// CHECK: CallExpr
-// CHECK: ImplicitCastExpr {{.*}} <FunctionToPointerDecay>
-// CHECK: 'double (int, float)' lvalue Function
-// CHECK: ImplicitCastExpr {{.*}} 'int' <FloatingToIntegral>
-// CHECK: 'float' lvalue ParmVar
-
-int process(float a, float b) {
-  return a + compute(a, b);
-}
-
-// CHECK-LABEL: FunctionDecl {{.*}} identity 'int (int)'
-// CHECK: ParmVarDecl {{.*}} 'int'
-// CHECK: ReturnStmt
-// CHECK: ImplicitCastExpr {{.*}} 'int' <LValueToRValue>
-// CHECK: 'int' lvalue ParmVar
-
-int identity(int v) {
-  return v;
-}
-
-// CHECK-LABEL: FunctionDecl {{.*}} multipleCasts 'double (int, int)'
-// CHECK: ParmVarDecl {{.*}} 'int'
-// CHECK: ParmVarDecl {{.*}} 'int'
-// CHECK: VarDecl {{.*}} 'float'
-// CHECK: BinaryOperator {{.*}} 'int' '+'
-// CHECK: 'int' lvalue ParmVar
-// CHECK: ImplicitCastExpr {{.*}} 'float' <IntegralToFloating>
-// CHECK: ReturnStmt
-// CHECK: BinaryOperator {{.*}} 'double' '+'
-// CHECK: ImplicitCastExpr {{.*}} 'double' <FloatingCast>
-// CHECK: 'float' lvalue Var
-// CHECK: FloatingLiteral {{.*}} 'double'
-
-double multipleCasts(int a, int b) {
-  float fa = a + b;
-  return fa + 1.0;
-}
-
-// CHECK-LABEL: FunctionDecl {{.*}} explicitCast 'float (int)'
-// CHECK: ParmVarDecl {{.*}} 'int'
-// CHECK: CStyleCastExpr {{.*}} 'float'
-// CHECK: 'int' lvalue ParmVar
-
-float explicitCast(int a) {
-  return (float)a;
-}
-
-// CHECK-LABEL: FunctionDecl {{.*}} returnFloat 'float (float)'
-// CHECK: ParmVarDecl {{.*}} 'float'
-// CHECK: ReturnStmt
-// CHECK: ImplicitCastExpr {{.*}} 'float' <LValueToRValue>
-// CHECK: 'float' lvalue ParmVar
-
-float returnFloat(float f) {
-  return f;
 }
