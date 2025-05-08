4290bc120d5ef92f55934a6a6cf3a787c0ed32e6
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2025-05-08 14:23:43 +0300

    test

--- clang/test/compiler-course/sedova/test.cpp
+++ clang/test/compiler-course/sedova/test.cpp
@@ -23,22 +23,50 @@ double compute(int x, float y) {
   return x + y;
 }
 
-// CHECK-LABEL: FunctionDecl {{.*}} multipleCasts 'double (int, int)'
-// CHECK: ParmVarDecl {{.*}} 'int'
-// CHECK: ParmVarDecl {{.*}} 'int'
-// CHECK: VarDecl {{.*}} 'float'
-// CHECK: ImplicitCastExpr {{.*}} 'float' <IntegralToFloating>
-// CHECK: BinaryOperator {{.*}} 'int' '+'
-// CHECK: 'int' lvalue ParmVar
-// CHECK: 'int' lvalue ParmVar
+// CHECK-LABEL: FunctionDecl {{.*}} process 'int (float, float)'
+// CHECK: ParmVarDecl {{.*}} 'float'
+// CHECK: ParmVarDecl {{.*}} 'float'
 // CHECK: ReturnStmt
+// CHECK: ImplicitCastExpr {{.*}} 'int' <FloatingToIntegral>
 // CHECK: BinaryOperator {{.*}} 'double' '+'
 // CHECK: ImplicitCastExpr {{.*}} 'double' <FloatingCast>
-// CHECK: ImplicitCastExpr {{.*}} 'float' <LValueToRValue>
-// CHECK: 'float' lvalue Var
-// CHECK: FloatingLiteral {{.*}} 'double'
+// CHECK: CallExpr
+// CHECK: ImplicitCastExpr {{.*}} <FunctionToPointerDecay>
+// CHECK: 'double (int, float)' lvalue Function
+// CHECK: ImplicitCastExpr {{.*}} 'int' <FloatingToIntegral>
+// CHECK: 'float' lvalue ParmVar
 
-double multipleCasts(int a, int b) {
-  float fa = a + b;
-  return fa + 1.0;
+int process(float a, float b) {
+  return a + compute(a, b);
+}
+
+// CHECK-LABEL: FunctionDecl {{.*}} identity 'int (int)'
+// CHECK: ParmVarDecl {{.*}} 'int'
+// CHECK: ReturnStmt
+// CHECK: ImplicitCastExpr {{.*}} 'int' <LValueToRValue>
+// CHECK: 'int' lvalue ParmVar
+
+int identity(int v) {
+  return v;
+}
+
+// CHECK-LABEL: FunctionDecl {{.*}} explicitCast 'float (int)'
+// CHECK: ParmVarDecl {{.*}} 'int'
+// CHECK: ReturnStmt
+// CHECK: CStyleCastExpr {{.*}} 'float'
+// CHECK: ImplicitCastExpr {{.*}} 'float' <IntegralToFloating>
+// CHECK: 'int' lvalue ParmVar
+
+float explicitCast(int a) {
+  return (float)a;
+}
+
+// CHECK-LABEL: FunctionDecl {{.*}} returnFloat 'float (float)'
+// CHECK: ParmVarDecl {{.*}} 'float'
+// CHECK: ReturnStmt
+// CHECK: ImplicitCastExpr {{.*}} 'float' <LValueToRValue>
+// CHECK: 'float' lvalue ParmVar
+
+float returnFloat(float f) {
+  return f;
 }
