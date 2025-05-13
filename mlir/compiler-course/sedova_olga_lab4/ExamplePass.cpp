8bedc2410cb5c132ac259396bab7043ba8ca01f2
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2025-05-13 18:49:49 +0300

    test

---
+++ mlir/compiler-course/sedova_olga_lab4/ExamplePass.cpp
@@ -0,0 +1,84 @@
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/SCF/IR/SCFOps.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/Function.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Pass/Pass.h"
+
+using namespace mlir;
+
+namespace {
+
+struct TraceLoopIterPass
+    : public PassWrapper<TraceLoopIterPass, OperationPass<ModuleOp>> {
+  void runOnOperation() override {
+    ModuleOp module = getOperation();
+    OpBuilder builder(module.getContext());
+
+    auto traceBeginFunc = module.lookupSymbol<FuncOp>("trace_loop_iter_begin");
+    auto traceEndFunc = module.lookupSymbol<FuncOp>("trace_loop_iter_end");
+
+    builder.setInsertionPointToStart(module.getBody());
+    if (!traceBeginFunc) {
+      traceBeginFunc = builder.create<FuncOp>(builder.getUnknownLoc(),
+                                              "trace_loop_iter_begin",
+                                              builder.getFunctionType({}, {}));
+      traceBeginFunc.setPrivate();
+    }
+    if (!traceEndFunc) {
+      traceEndFunc =
+          builder.create<FuncOp>(builder.getUnknownLoc(), "trace_loop_iter_end",
+                                 builder.getFunctionType({}, {}));
+      traceEndFunc.setPrivate();
+    }
+
+    module.walk([&](Operation *op) {
+      if (auto affineFor = dyn_cast<affine::AffineForOp>(op)) {
+        insertTraceCalls(affineFor, traceBeginFunc, traceEndFunc);
+      }
+      else if (auto scfFor = dyn_cast<scf::ForOp>(op)) {
+        insertTraceCalls(scfFor, traceBeginFunc, traceEndFunc);
+      }
+      else if (auto scfWhile = dyn_cast<scf::WhileOp>(op)) {
+        insertTraceCalls(scfWhile, traceBeginFunc, traceEndFunc);
+      }
+    });
+  }
+
+private:
+  template <typename LoopOp>
+  void insertTraceCalls(LoopOp loopOp, FuncOp traceBeginFunc,
+                        FuncOp traceEndFunc) {
+    OpBuilder builder(loopOp.getBody());
+    builder.setInsertionPointToStart(&loopOp.getBody()->front());
+    builder.create<func::CallOp>(loopOp.getLoc(), traceBeginFunc,
+                                 ArrayRef<Value>{});
+
+    builder.setInsertionPoint(loopOp.getBody()->getTerminator());
+    builder.create<func::CallOp>(loopOp.getLoc(), traceEndFunc,
+                                 ArrayRef<Value>{});
+  }
+
+  void insertTraceCalls(scf::WhileOp whileOp, FuncOp traceBeginFunc,
+                        FuncOp traceEndFunc) {
+    Block &afterBlock = whileOp.getAfter().front();
+    OpBuilder builder(&afterBlock);
+    builder.setInsertionPointToStart(&afterBlock);
+    builder.create<func::CallOp>(whileOp.getLoc(), traceBeginFunc,
+                                 ArrayRef<Value>{});
+    builder.setInsertionPoint(afterBlock.getTerminator());
+    builder.create<func::CallOp>(whileOp.getLoc(), traceEndFunc,
+                                 ArrayRef<Value>{});
+  }
+};
+
+}
+
+namespace mlir {
+void registerTraceLoopIterPass() {
+  PassRegistration<TraceLoopIterPass>(
+      "trace-loop-iter", "Insert calls to @trace_loop_iter_begin and "
+                         "@trace_loop_iter_end on loop iterations");
+}
+}
---
+++ mlir/test/compiler-course/sedova_olga_lab4/test.mlir
@@ -0,0 +1,61 @@
+// RUN: mlir-opt -load-pass-plugin=%mlir_lib_dir/Lab4_Sedova_Olga_FIIT1_MLIR%shlibext \
+// RUN: --pass-pipeline="builtin.module(Lab4_Sedova_Olga_FIIT1_MLIR)" %s | FileCheck %s
+
+// CHECK-LABEL: func @test_affine
+// CHECK: func.call @trace_loop_iter_begin()
+// CHECK: affine.for
+// CHECK: func.call @trace_loop_iter_end()
+
+// CHECK-LABEL: func @test_scf_for
+// CHECK: func.call @trace_loop_iter_begin()
+// CHECK: scf.for
+// CHECK: func.call @trace_loop_iter_end()
+
+// CHECK-LABEL: func @test_scf_while
+// CHECK: func.call @trace_loop_iter_begin()
+// CHECK: scf.while
+// CHECK: func.call @trace_loop_iter_end()
+
+func.func @trace_loop_iter_begin() {
+  return
+}
+
+func.func @trace_loop_iter_end() {
+  return
+}
+
+func.func @test_affine(%arg0: memref<10xf32>) {
+  affine.for %i = 0 to 10 {
+    // trace_loop_iter_begin should be inserted here by the pass
+    %val = affine.load %arg0[%i] : memref<10xf32>
+    // trace_loop_iter_end should be inserted here by the pass
+  }
+  return
+}
+
+func.func @test_scf_for(%arg0: memref<10xf32>) {
+  scf.for %i = 0 to 10 step 1 {
+    // trace_loop_iter_begin should be inserted here by the pass
+    %val = memref.load %arg0[%i] : memref<10xf32>
+    // trace_loop_iter_end should be inserted here by the pass
+  }
+  return
+}
+
+func.func @test_scf_while(%arg0: memref<10xf32>) {
+  %c0 = arith.constant 0 : index
+  %c10 = arith.constant 10 : index
+  scf.while (%iv = %c0) : (index) -> () {
+    scf.condition {
+      %cond = arith.cmpi slt, %iv, %c10 : index
+      scf.yield %cond : i1
+    } do {
+      // trace_loop_iter_begin should be inserted here by the pass
+      %val = memref.load %arg0[%iv] : memref<10xf32>
+      // trace_loop_iter_end should be inserted here by the pass
+      %iv_next = arith.addi %iv, %c1 : index
+      scf.yield
+    }
+  }
+  return
+}
