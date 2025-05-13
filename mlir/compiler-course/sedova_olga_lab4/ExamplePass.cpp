5e0bf8289dc14b1df3fb36f2313783ec6c25f3e6
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2025-05-13 19:36:00 +0300

    test

--- mlir/compiler-course/sedova_olga_lab4/ExamplePass.cpp
+++ mlir/compiler-course/sedova_olga_lab4/ExamplePass.cpp
@@ -1,12 +1,13 @@
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/IR/Builders.h"
-#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Pass/Pass.h"
 
 using namespace mlir;
+using namespace mlir::func;
 
 namespace {
 
@@ -19,28 +20,28 @@ struct TraceLoopIterPass
     auto traceBeginFunc = module.lookupSymbol<FuncOp>("trace_loop_iter_begin");
     auto traceEndFunc = module.lookupSymbol<FuncOp>("trace_loop_iter_end");
 
-    builder.setInsertionPointToStart(module.getBody());
     if (!traceBeginFunc) {
-      traceBeginFunc = builder.create<FuncOp>(builder.getUnknownLoc(),
-                                              "trace_loop_iter_begin",
-                                              builder.getFunctionType({}, {}));
+      auto loc = builder.getUnknownLoc();
+      auto funcType = builder.getFunctionType({}, {});
+      traceBeginFunc = FuncOp::create(loc, "trace_loop_iter_begin", funcType);
+      module.push_back(traceBeginFunc);
       traceBeginFunc.setPrivate();
     }
+
     if (!traceEndFunc) {
-      traceEndFunc =
-          builder.create<FuncOp>(builder.getUnknownLoc(), "trace_loop_iter_end",
-                                 builder.getFunctionType({}, {}));
+      auto loc = builder.getUnknownLoc();
+      auto funcType = builder.getFunctionType({}, {});
+      traceEndFunc = FuncOp::create(loc, "trace_loop_iter_end", funcType);
+      module.push_back(traceEndFunc);
       traceEndFunc.setPrivate();
     }
 
     module.walk([&](Operation *op) {
       if (auto affineFor = dyn_cast<affine::AffineForOp>(op)) {
         insertTraceCalls(affineFor, traceBeginFunc, traceEndFunc);
-      }
-      else if (auto scfFor = dyn_cast<scf::ForOp>(op)) {
+      } else if (auto scfFor = dyn_cast<scf::ForOp>(op)) {
         insertTraceCalls(scfFor, traceBeginFunc, traceEndFunc);
-      }
-      else if (auto scfWhile = dyn_cast<scf::WhileOp>(op)) {
+      } else if (auto scfWhile = dyn_cast<scf::WhileOp>(op)) {
         insertTraceCalls(scfWhile, traceBeginFunc, traceEndFunc);
       }
     });
@@ -75,11 +76,11 @@ private:
 
 }
 
-namespace {
+namespace mlir {
+void registerTraceLoopIterPass() {
+}
+} // namespace mlir
+
 static mlir::PassRegistration<TraceLoopIterPass>
     pass("trace-loop-iter", "Insert calls to @trace_loop_iter_begin and "
                             "@trace_loop_iter_end on loop iterations");
-}
-
-void mlir::registerTraceLoopIterPass() {
-}
