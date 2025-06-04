8d2326248bb22f739dbe57e5dfc56672d9e865a9
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2025-06-04 16:19:32 +0300

    test

--- llvm/compiler-course/llvm-ir/sedova/ExamplePass.cpp
+++ llvm/compiler-course/llvm-ir/sedova/ExamplePass.cpp
@@ -11,22 +11,26 @@ using namespace llvm;
 namespace {
 struct ReplaceAddWithCall : PassInfoMixin<ReplaceAddWithCall> {
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
-    std::string addFuncName;
-    if (F.getReturnType()->isIntegerTy(32))
-      addFuncName = "add_i32";
-    else if (F.getReturnType()->isIntegerTy(64))
-      addFuncName = "add_i64";
-    else if (F.getReturnType()->isFloatTy())
-      addFuncName = "add_float";
-    else
+    Module *M = F.getParent();
+    if (!M)
       return PreservedAnalyses::all();
+
+    std::string addFuncName;
+    bool isFloat = false;
+
+    Type *retType = F.getReturnType();
+    if (retType->isIntegerTy(32))
+      addFuncName = "add_i32";
+    else if (retType->isIntegerTy(64))
+      addFuncName = "add_i64";
+    else if (retType->isFloatTy()) {
+      addFuncName = "add_float";
+      isFloat = true;
+    } else {
+      return PreservedAnalyses::all();
+    }
 
     Function *addFunc = M->getFunction(addFuncName);
-    if (!addFunc)
-      return PreservedAnalyses::all();
-
-    Module *M = F.getParent();
-    Function *addFunc = M->getFunction("add");
     if (!addFunc || addFunc->isDeclaration())
       return PreservedAnalyses::all();
 
@@ -44,7 +48,8 @@ struct ReplaceAddWithCall : PassInfoMixin<ReplaceAddWithCall> {
       for (auto instIter = BB.begin(), end = BB.end(); instIter != end;) {
         Instruction *I = &*instIter++;
         if (auto *binOp = dyn_cast<BinaryOperator>(I)) {
-          if (binOp->getOpcode() == Instruction::Add) {
+          if ((!isFloat && binOp->getOpcode() == Instruction::Add) ||
+              (isFloat && binOp->getOpcode() == Instruction::FAdd)) {
             Value *op1 = binOp->getOperand(0);
             Value *op2 = binOp->getOperand(1);
 
@@ -63,7 +68,6 @@ struct ReplaceAddWithCall : PassInfoMixin<ReplaceAddWithCall> {
     return changed ? PreservedAnalyses::none() : PreservedAnalyses::all();
   }
 
-
   static bool isRequired() { return true; }
 };
 } // namespace
@@ -75,7 +79,7 @@ llvmGetPassPluginInfo() {
             PB.registerPipelineParsingCallback(
                 [](StringRef name, FunctionPassManager &FPM,
                    ArrayRef<PassBuilder::PipelineElement>) -> bool {
-                  if (name == "replace-add") { // Имя pass в нижнем регистре
+                  if (name == "replace-add") {
                     FPM.addPass(ReplaceAddWithCall{});
                     return true;
                   }
