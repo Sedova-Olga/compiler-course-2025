433fab7504a09b801452de37b82d0dea08fbd1b5
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2025-06-05 15:02:50 +0300

    test

--- llvm/compiler-course/backend/sedova_o_lab3/ExamplePass.cpp
+++ llvm/compiler-course/backend/sedova_o_lab3/ExamplePass.cpp
@@ -2,6 +2,7 @@
 #include "llvm/CodeGen/MachineFunctionPass.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
 #include "llvm/CodeGen/MachineRegisterInfo.h"
+#include "llvm/CodeGen/TargetInstrInfo.h"
 #include "llvm/InitializePasses.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/Debug.h"
@@ -14,47 +15,37 @@ namespace {
 class FMADecomposePass : public MachineFunctionPass {
 public:
   static char ID;
-  FMADecomposePass() : MachineFunctionPass(ID) {
-    initializeFMADecomposePassPass(*PassRegistry::getPassRegistry());
-  }
+  FMADecomposePass() : MachineFunctionPass(ID) {}
 
   bool runOnMachineFunction(MachineFunction &MF) override {
     bool Changed = false;
     MachineRegisterInfo &MRI = MF.getRegInfo();
 
+    const TargetInstrInfo *TII = MF.getSubtarget().getInstrInfo();
+
     for (auto &MBB : MF) {
       for (auto MII = MBB.begin(), E = MBB.end(); MII != E;) {
         MachineInstr &MI = *MII++;
-        // Проверяем, что инструкция — FMA
-        if (MI.getOpcode() == TargetOpcode::FMA) {
+        if (MI.getOpcode() == TargetOpcode::G_FMA) {
           LLVM_DEBUG(dbgs() << "Decomposing FMA: " << MI);
 
-          // Предполагаем, что операнды: dst, src1, src2, src3
-          // FMA dst, src1, src2, src3  => dst = src3 + (src1 * src2)
           Register Dst = MI.getOperand(0).getReg();
           Register Src1 = MI.getOperand(1).getReg();
           Register Src2 = MI.getOperand(2).getReg();
           Register Src3 = MI.getOperand(3).getReg();
 
-          // Создаём временный регистр для MUL
           Register MulReg = MRI.createVirtualRegister(MRI.getRegClass(Dst));
 
-          // Вставляем MUL перед FMA
-          BuildMI(MBB, MI, MI.getDebugLoc(),
-                  MF.getSubtarget().getInstrInfo()->get(TargetOpcode::MUL),
+          BuildMI(MBB, MI, MI.getDebugLoc(), TII->get(TargetOpcode::G_MUL),
                   MulReg)
               .addReg(Src1)
               .addReg(Src2);
 
-          // Вставляем ADD перед FMA
-          BuildMI(MBB, MI, MI.getDebugLoc(),
-                  MF.getSubtarget().getInstrInfo()->get(TargetOpcode::ADD), Dst)
+          BuildMI(MBB, MI, MI.getDebugLoc(), TII->get(TargetOpcode::G_ADD), Dst)
               .addReg(Src3)
               .addReg(MulReg);
 
-          // Удаляем исходную FMA
           MI.eraseFromParent();
-
           Changed = true;
         }
       }
@@ -70,6 +61,8 @@ char FMADecomposePass::ID = 0;
 
 } // end anonymous namespace
 
-static RegisterPass<FMADecomposePass> X("fma-decompose", "Decompose FMA into MUL + ADD",
-                                        false,
-                                   false);
+INITIALIZE_PASS(FMADecomposePass, "fma-decompose",
+                "Decompose FMA into MUL + ADD", false, false)
+
+static RegisterPass<FMADecomposePass>
+    X("fma-decompose", "Decompose FMA into MUL + ADD", false, false);
