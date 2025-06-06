20a3d431511da54cd14c520936c837ef24620cca
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2025-06-06 14:18:25 +0300

    test

--- llvm/compiler-course/backend/sedova_o_lab3/ExamplePass.cpp
+++ llvm/compiler-course/backend/sedova_o_lab3/ExamplePass.cpp
@@ -6,142 +6,134 @@
 #include "llvm/CodeGen/MachineFunctionPass.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
 #include "llvm/CodeGen/MachineRegisterInfo.h"
-#include "llvm/CodeGen/TargetInstrInfo.h"
-#include "llvm/Support/Debug.h"
-#include "llvm/Support/ErrorHandling.h"
-#include <optional>
+#include "llvm/CodeGen/TargetOpcodes.h"
+#include "llvm/Passes/PassBuilder.h"
 
-#define DEBUG_TYPE "fma-decompose-x86"
-
-using namespace llvm;
-using std::optional;
+#define DEBUG_TYPE "fma-decompose"
 
 namespace {
 
-struct FMAGroup {
-  ArrayRef<unsigned> Opcodes;
-  unsigned MulOpcode;
-  unsigned AddOpcode;
+struct FMAOpcodeGroup {
+  unsigned FMAOps[3]; // 132, 213, 231
+  unsigned MulOp;
+  unsigned AddOp;
 };
 
-static const FMAGroup FMA_Groups[] = {
-    {{X86::VFMADD132PSr, X86::VFMADD213PSr, X86::VFMADD231PSr},
-     X86::MULPSrr,
-     X86::ADDPSrr},
-    {{X86::VFMADD132PDr, X86::VFMADD213PDr, X86::VFMADD231PDr},
-     X86::MULPDrr,
-     X86::ADDPDrr},
-    {{X86::VFMADD132SSr, X86::VFMADD213SSr, X86::VFMADD231SSr},
-     X86::MULSSrr,
-     X86::ADDSSrr},
-    {{X86::VFMADD132SDr, X86::VFMADD213SDr, X86::VFMADD231SDr},
-     X86::MULSDrr,
-     X86::ADDSDrr},
+static const FMAOpcodeGroup FMAOpcodes[] = {
+    {{llvm::X86::VFMADD132PSr, llvm::X86::VFMADD213PSr,
+      llvm::X86::VFMADD231PSr},
+     llvm::X86::MULPSrr,
+     llvm::X86::ADDPSrr},
+    {{llvm::X86::VFMADD132PDr, llvm::X86::VFMADD213PDr,
+      llvm::X86::VFMADD231PDr},
+     llvm::X86::MULPDrr,
+     llvm::X86::ADDPDrr},
+    {{llvm::X86::VFMADD132SSr, llvm::X86::VFMADD213SSr,
+      llvm::X86::VFMADD231SSr},
+     llvm::X86::MULSSrr,
+     llvm::X86::ADDSSrr},
+    {{llvm::X86::VFMADD132SDr, llvm::X86::VFMADD213SDr,
+      llvm::X86::VFMADD231SDr},
+     llvm::X86::MULSDrr,
+     llvm::X86::ADDSDrr},
 };
 
-class FMADecomposePass : public MachineFunctionPass {
+bool matchFMA(unsigned Opc, unsigned &MulOp, unsigned &AddOp,
+              unsigned &FMAIndex) {
+  for (const auto &Group : FMAOpcodes) {
+    for (unsigned i = 0; i < 3; ++i) {
+      if (Group.FMAOps[i] == Opc) {
+        MulOp = Group.MulOp;
+        AddOp = Group.AddOp;
+        FMAIndex = i; // 0 = 132, 1 = 213, 2 = 231
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+void decomposeFMA(llvm::MachineInstr &MI, llvm::MachineBasicBlock &MBB,
+                  const llvm::X86InstrInfo *TII, llvm::MachineRegisterInfo &MRI,
+                  unsigned MulOp, unsigned AddOp, unsigned FMAIndex) {
+
+  const llvm::DebugLoc &DL = MI.getDebugLoc();
+
+  llvm::Register Dst = MI.getOperand(0).getReg();
+  llvm::Register Op1 = MI.getOperand(1).getReg();
+  llvm::Register Op2 = MI.getOperand(2).getReg();
+  llvm::Register Op3 = MI.getOperand(3).getReg();
+
+  llvm::Register MulLHS, MulRHS, AddSrc;
+
+  // FMA semantics:
+  // 132: (Op1 * Op3) + Op2
+  // 213: (Op1 * Op2) + Op3
+  // 231: (Op2 * Op3) + Op1
+
+  switch (FMAIndex) {
+  case 0: // 132
+    MulLHS = Op1;
+    MulRHS = Op3;
+    AddSrc = Op2;
+    break;
+  case 1: // 213
+    MulLHS = Op1;
+    MulRHS = Op2;
+    AddSrc = Op3;
+    break;
+  case 2: // 231
+    MulLHS = Op2;
+    MulRHS = Op3;
+    AddSrc = Op1;
+    break;
+  default:
+    llvm_unreachable("Invalid FMA index");
+  }
+
+  const llvm::TargetRegisterClass *RC = MRI.getRegClass(MulLHS);
+  llvm::Register Tmp = MRI.createVirtualRegister(RC);
+
+  llvm::BuildMI(MBB, MI, DL, TII->get(MulOp), Tmp)
+      .addReg(MulLHS)
+      .addReg(MulRHS);
+
+  llvm::BuildMI(MBB, MI, DL, TII->get(AddOp), Dst).addReg(AddSrc).addReg(Tmp);
+}
+
+class FMADecomposePass : public llvm::MachineFunctionPass {
 public:
   static char ID;
-  FMADecomposePass() : MachineFunctionPass(ID) {}
+  FMADecomposePass() : llvm::MachineFunctionPass(ID) {}
 
-  bool runOnMachineFunction(MachineFunction &MF) override {
-    const X86Subtarget &ST = MF.getSubtarget<X86Subtarget>();
-    if (!ST.hasFMA()) {
-      LLVM_DEBUG(dbgs() << "FMA not supported on this subtarget\n");
+  bool runOnMachineFunction(llvm::MachineFunction &MF) override {
+    const llvm::X86Subtarget &ST = MF.getSubtarget<llvm::X86Subtarget>();
+    if (!ST.hasFMA())
       return false;
-    }
 
-    const X86InstrInfo *TII = ST.getInstrInfo();
-    MachineRegisterInfo &MRI = MF.getRegInfo();
+    const llvm::X86InstrInfo *TII = ST.getInstrInfo();
+    llvm::MachineRegisterInfo &MRI = MF.getRegInfo();
     bool Changed = false;
 
-    for (MachineBasicBlock &MBB : MF) {
-      SmallVector<MachineInstr *, 8> ToErase;
+    for (llvm::MachineBasicBlock &MBB : MF) {
+      llvm::SmallVector<llvm::MachineInstr *, 8> ToErase;
 
-      for (MachineInstr &MI : make_early_inc_range(MBB)) {
-        auto FMAInfoOpt = getFMAInfo(MI.getOpcode());
-        if (!FMAInfoOpt)
+      for (llvm::MachineInstr &MI : llvm::make_early_inc_range(MBB)) {
+        unsigned MulOp, AddOp, FMAIndex;
+        if (!matchFMA(MI.getOpcode(), MulOp, AddOp, FMAIndex))
           continue;
 
-        Changed |= decomposeFMA(MI, MBB, TII, MRI, *FMAInfoOpt);
+        decomposeFMA(MI, MBB, TII, MRI, MulOp, AddOp, FMAIndex);
         ToErase.push_back(&MI);
+        Changed = true;
       }
 
-      for (auto *MI : ToErase)
+      for (llvm::MachineInstr *MI : ToErase) {
         MI->eraseFromParent();
+      }
     }
 
     return Changed;
-  }
-
-private:
-  struct FMAData {
-    unsigned MulOp;
-    unsigned AddOp;
-    unsigned FMAIndex;
-  };
-
-  optional<FMAData> getFMAInfo(unsigned Opcode) const {
-    for (const auto &Group : FMA_Groups) {
-      for (unsigned i = 0; i < Group.Opcodes.size(); ++i) {
-        if (Group.Opcodes[i] == Opcode) {
-          return FMAData{Group.MulOpcode, Group.AddOpcode, i};
-        }
-      }
-    }
-    return std::nullopt;
-  }
-
-  bool decomposeFMA(MachineInstr &MI, MachineBasicBlock &MBB,
-                    const X86InstrInfo *TII, MachineRegisterInfo &MRI,
-                    const FMAData &Info) {
-    DebugLoc DL = MI.getDebugLoc();
-
-    Register Dst = MI.getOperand(0).getReg();
-    Register Op1 = MI.getOperand(1).getReg();
-    Register Op2 = MI.getOperand(2).getReg();
-    Register Op3 = MI.getOperand(3).getReg();
-
-    Register MulLHS, MulRHS, AddSrc;
-
-    switch (Info.FMAIndex) {
-    case 0: // 132
-      MulLHS = Op1;
-      MulRHS = Op3;
-      AddSrc = Op2;
-      break;
-    case 1: // 213
-      MulLHS = Op1;
-      MulRHS = Op2;
-      AddSrc = Op3;
-      break;
-    case 2: // 231
-      MulLHS = Op2;
-      MulRHS = Op3;
-      AddSrc = Op1;
-      break;
-    default:
-      llvm_unreachable("Invalid FMA index");
-    }
-    const TargetRegisterClass *RC = MRI.getRegClassOrNull(MulLHS);
-    if (!RC) {
-      LLVM_DEBUG(dbgs() << "Failed to get register class for MulLHS\n");
-      return false;
-    }
-
-    Register TmpReg = MRI.createVirtualRegister(RC);
-
-    BuildMI(MBB, MI, DL, TII->get(Info.MulOp), TmpReg)
-        .addReg(MulLHS)
-        .addReg(MulRHS)
-        .setMIFlag(MachineInstr::MIFlag::NoFPExcept);
-
-    BuildMI(MBB, MI, DL, TII->get(Info.AddOp), Dst)
-        .addReg(AddSrc)
-        .addReg(TmpReg)
-        .setMIFlag(MachineInstr::MIFlag::NoFPExcept);
-
-    return true;
   }
 };
 
@@ -149,6 +141,6 @@ char FMADecomposePass::ID = 0;
 
 } // namespace
 
-static RegisterPass<FMADecomposePass>
-    X("fma-decompose-x86", "Decompose x86 FMA instructions into MUL + ADD",
-      false, false);
+static llvm::RegisterPass<FMADecomposePass>
+    X("fma-decompose-x86",
+      "Decomposes single FMA instructions into MUL and ADD ones", false, false);
